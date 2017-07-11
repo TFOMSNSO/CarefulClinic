5115fa06d40d820a8ba7cf2ff8d4a3b10416da50
Sergey Pylypiv <gysek@yandex.ru>
2017-07-11 22:32:30 +0700

    dev

--- client/src/src/app/header/header.component.ts
+++ client/src/src/app/header/header.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { trigger,style,transition,animate,keyframes,query,stagger, state } from '@angular/animations';
 
 @Component({
     selector:'app-header',
     templateUrl: './header.component.html',
-  	styleUrls: ['./header.component.scss']
+  	styleUrls: ['./header.component.scss'],
+  	  animations: [
+	trigger('flyInOut', [
+            state('in', style({ opacity: 1, transform: 'translateX(0)' })),
+            transition('void => *', [
+                style({
+                    opacity: 0,
+                    transform: 'translateX(-100%)'
+                }),
+                animate('0.6s ease-in')
+            ]),
+            transition('* => void', [
+                animate('0.2s 0.1s ease-out', style({
+                    opacity: 0,
+                    transform: 'translateX(100%)'
+                }))
+            ])
+    ])
+    
+    ]
 })
 export class HeaderComponent{
-    }
+  
+}
